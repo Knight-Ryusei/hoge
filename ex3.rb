206e05f090d36ba55a9fc6fb624c33f9a1d24a38
Ryusei <ryusei0053@gmail.com>
2015-11-05 16:41:05 +0900

    first commit

---
+++ ex3.rb
@@ -0,0 +1,30 @@
+loop{
+a,op,b=STDIN.gets.split
+  
+  a=a.to_i
+  b=b.to_i
+  
+  case op
+    
+  when '+' then
+    
+    puts a+b
+    
+  when'-'then
+    
+    puts a-b
+    
+  when'*'then
+    
+    puts a*b
+    
+  when'/'then
+   
+    puts a/b
+    
+  when '?'then
+    break
+  
+  end
+}
+
---
+++ ex4.rb
@@ -0,0 +1,7 @@
+
+i=0;
+
+while i<1000 do 
+puts "Hello World" 
+i=i+1
+end
